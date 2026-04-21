0dd9acefde7c8ed6657821f50610fdef23b7befb
rzhandos256 <r.zhandos256@gmail.com>
2026-04-21 16:13:04 +0500

    aliasses

---
+++ .aliass.zsh
@@ -0,0 +1,36 @@
+alias cls="clear"
+alias quit="exit"
+alias cat="bat"
+alias o="open"
+alias c="pbcopy"
+alias hh="tldr"
+alias t="tree"
+alias ac="source ~/files/ac.sh"
+alias ca="deactivate"
+
+# zsh
+alias zconf="vi ~/.zshrc"
+alias zupd="source ~/.zshrc"
+
+# vi
+alias vconf="vi ~/.vimrc"
+
+# vi
+alias aliass="vi ~/.aliass.zsh"
+
+# skhd
+alias skconf="vi ~/.config/skhd/skhdrc"
+
+# tmux
+alias tt="tmux new -s "
+alias ta="tmux attach -t "
+alias tks="tmux kill-server"
+alias tls="tmux ls"
+alias tconf="vi ~/.tmux.conf"
+alias tupd="tmux source-file ~/.tmux.conf"
+
+# alacritty
+alias aconf="vi ~/.config/alacritty/alacritty.toml"
+
+# env
+alias envv="vi ~/.env.zsh"

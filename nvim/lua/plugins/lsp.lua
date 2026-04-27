7e48f65975150ee89cb3c6226aeec7756804d90d
rzhandos256 <r.zhandos256@gmail.com>
2026-04-27 13:17:08 +0500

    nvim upd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,13 +1,11 @@
 return {
 	"neovim/nvim-lspconfig",
-	-- event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
 		{ "williamboman/mason.nvim", opts = {} },
 		{
 			"williamboman/mason-lspconfig.nvim",
 			opts = {
 				ensure_installed = { "lua_ls", "pyright", "bashls", "dockerls" },
-				automatic_enable = true,
 			},
 		},
 		"hrsh7th/cmp-nvim-lsp",
@@ -16,10 +14,51 @@ return {
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		local servers = { "lua_ls", "pyright", "bashls", "dockerls" }
 
-		-- Только инициализация и включение серверов
+		-- Настройка через новый API Neovim 0.11
 		for _, server in ipairs(servers) do
-			vim.lsp.config(server, { capabilities = capabilities })
+			local config = {
+				capabilities = capabilities,
+			}
+
+			-- Специфические настройки для Lua
+			if server == "lua_ls" then
+				config.settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						workspace = {
+							preloadFileSize = 10000,
+							library = {
+								vim.env.VIMRUNTIME,
+							},
+						},
+					},
+				}
+			end
+
+			-- НОВЫЙ МЕТОД: Регистрация и включение
+			vim.lsp.config(server, config)
 			vim.lsp.enable(server)
 		end
+
+		-- ГОРЯЧИЕ КЛАВИШИ
+		-- Используем LspAttach, чтобы бинды работали только там, где есть сервер
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local opts = { buffer = args.buf, silent = true }
+
+				-- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				-- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+				-- Форматирование (удобно для Python/Lua)
+				vim.keymap.set("n", "<leader>rr", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
+			end,
+		})
 	end,
 }
